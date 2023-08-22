c43576b4f2870262cfde9b62a6dc3063d169d328
esramobarak <esramobaruk@gmail.com>
2023-08-22 10:12:05 -0700

    z do

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+int _putchar(char c);
+void _puts(char *str);
+void reset_to_98(int *n);
+void swap_int(int *a, int *b);
+void print_array(int *a, int n);
+void rev_string(char *s);
+int _strlen(char *s);
+void print_rev(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+char *_strcpy(char *dest, char *src);
+#endif /* MAIN_H */
